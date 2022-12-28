efd51f96a31d959b89165a2f454b0ef14121c186
iqbal167 <muhammadiqbalali167@gmail.com>
2022-12-28 17:42:32 +0700

    add: swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
   app.enableCors();
   app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get('app.port'));
 }
